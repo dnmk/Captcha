22739faec8b1b844fe34adff36a5bd8960003dc5
nate1k <matousek.dominik@icloud.com>
2017-02-01 01:13:14 +0100

    Main files

---
+++ captcha.php
@@ -0,0 +1,25 @@
+<?php
+    session_start();
+
+    $string = '';
+    $dir = '/fonts/';
+
+    //Create random string!
+    for ($i = 0; $i < 5; $i++){
+        $string .= chr(rand(97, 122));
+    }
+
+    // random code is now my output string
+    $_SESSION['random_code'] = $string;
+
+    $image = imagecreatetruecolor(136, 50);
+    $black = imagecolorallocate($image, 0, 0, 0);
+    $color = imagecolorallocate($image, 200, 100, 90);
+    $white = imagecolorallocate($image, 255, 255, 255);
+
+    imagefilledrectangle($image, 0, 0, 200, 100, $white); //Rectangle size
+    imagettftext($image, 30, 0, 10, 40, $color, $dir."MonospaceTypewriter.ttf", $_SESSION['random_code']); //$dir."Your font"
+
+    header("Content-type: image/png");
+    imagepng($image);
+?>
